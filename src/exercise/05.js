35932bf11a16fb59b9960c2ea5d20eeacce95261
Vee Uye <vee@accommodationforstudents.com>
2022-11-30 13:42:42 +0000

    chore: test commit

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,7 +4,7 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 🐨 add a className prop to each div and apply the correct class names
+// 🐨 add a className prop to each div and apply the correct class names...
 // based on the text content
 // 💰 Here are the available class names: box, box--large, box--medium, box--small
 // 💰 each of the elements should have the "box" className applied
