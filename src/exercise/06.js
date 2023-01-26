0b09433e36f61ad7844a45e79f91ee6c2d52ee71
Vee Uye <vee.uye@gmail.com>
2023-01-26 12:15:52 +0000

    programmatically control input value

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,57 +1,50 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
+// 🐨 add a submit event handler here (`handleSubmit`).
+// 💰 Make sure to accept the `event` as an argument and call
+// `event.preventDefault()` to prevent the default behavior of form submit
+// events (which refreshes the page).
+// 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+//
+// 🐨 get the value from the username input (using whichever method
+// you prefer from the options mentioned in the instructions)
+// 💰 For example: event.target.elements[0].value
+// 🐨 Call `onSubmitUsername` with the value of the input
+
+// 🐨 add the onSubmit handler to the <form> below
+
+// 🐨 make sure to associate the label to the input.
+// to do so, set the value of 'htmlFor' prop of the label to the id of input
+
 import * as React from 'react'
-import {useRef, useState} from 'react'
+import {useState} from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  const [error, setError] = useState()
+  const [userName, setUserName] = useState('')
 
-  const inputRef = useRef(null)
   const handleSubmit = event => {
     event.preventDefault()
-    // const value = event.target.elements.firstNameInput.value
-    const value = inputRef.current.value
-    onSubmitUsername(value)
+    onSubmitUsername(userName)
   }
-
-  let disableButton = false
 
   const handleChange = event => {
     const {value} = event.target
-    const isValid = value === value.toLowerCase()
-    setError(isValid ? null : 'Username must be in lower case')
+    setUserName(value.toLowerCase())
   }
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
 
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="userNameInput">Username:</label>
         <input
-          ref={inputRef}
           id="userNameInput"
           type="text"
           onChange={handleChange}
+          value={userName}
         />
       </div>
-      <div style={{color: 'red'}}>{error}</div>
-      <button type="submit" disabled={Boolean(error)}>
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
